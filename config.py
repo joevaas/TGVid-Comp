4a71b475ffc88949ecff83b44dbb6d293baa1895
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2025-01-01 21:47:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class Config(object):
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
-    PORT = int(os.environ.get("PORT", "8030"))
+    PORT = int(os.environ.get("PORT", "8080"))
     WARN = """❌ You Can't Able to use the Bot"""
 
 
